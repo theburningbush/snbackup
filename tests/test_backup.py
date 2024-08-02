ae0403f145bc783e161c9db18730ab8f5b87d24f
devin <devinwrx@gmail.com>
2024-08-02 14:35:37 -0400

    setting up tests

--- tests/test_backup.py
+++ tests/test_backup.py
@@ -1,2 +1,5 @@
-# Pytest tests
+from snbackup import backup
 
+
+def test_nothing_yet():
+    assert None is None
--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,1 +1,4 @@
-# Pytest
+from snbackup.notes import Note
+
+def test_nothing_yet():
+    assert 'yes' == 'yes'
