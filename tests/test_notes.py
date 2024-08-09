14f2bd62f2c03d54f29e9810860e6ce701049efe
devin <devinwrx@gmail.com>
2024-08-09 17:22:25 -0400

    more tests for notes for when i break things eventually

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,11 +1,14 @@
-from snbackup.notes import Note
-from pathlib import Path
-
 import pytest
+from pathlib import Path
+from datetime import datetime
+
+from snbackup.notes import Note
+from snbackup.notes import BadDateError
+
 
 @pytest.fixture
 def some_note() -> Note:
-    return Note(Path('/test/path/2024-08-01'),'uri/fake.note', '2024-07-04 13:45:01', 404040)
+    return Note(Path('/test/path/2024-08-01'), 'uri/fake.note', '2024-07-04 13:45:01', 404040)
 
 
 def test_save_date(some_note):
@@ -28,3 +31,28 @@ def test_file_bytes_raises_typerror(some_note):
 def test_file_bytes_sets_properly(some_note):
     some_note.file_bytes = b'actual_bytes_object'
     some_note.file_bytes == b'actual_bytes_object'
+
+
+def test_last_modified(some_note):
+    assert some_note.last_modified == datetime.fromisoformat('2024-07-04 13:45:01')
+
+
+def test_last_modified_raises_baddateerror(some_note):
+    with pytest.raises(BadDateError):
+        some_note.last_modified = 999
+
+    with pytest.raises(BadDateError):
+        some_note.last_modified = 'bad_value'
+
+    assert some_note.last_modified == datetime(2000, 1, 1)
+
+
+def test_make_record(some_note):
+    record = {
+        'saved': '2024-08-01',
+        'current_loc': '/test/path/2024-08-01',
+        'uri': 'uri/fake.note',
+        'modified': '2024-07-04 13:45:01',
+        'size': 404040,
+    }
+    assert some_note.make_record() == record
