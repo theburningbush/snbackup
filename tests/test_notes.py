864cc9ba4f7d5565bb30ffa65b73d0046dd9d29f
devin <devinwrx@gmail.com>
2024-08-09 17:01:03 -0400

    adding more tests

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,4 +1,30 @@
 from snbackup.notes import Note
+from pathlib import Path
 
-def test_nothing_yet():
-    assert 'yes' == 'yes'
+import pytest
+
+@pytest.fixture
+def some_note() -> Note:
+    return Note(Path('/test/path/2024-08-01'),'uri/fake.note', '2024-07-04 13:45:01', 404040)
+
+
+def test_save_date(some_note):
+    assert some_note.save_date == '2024-08-01'
+
+
+def test_full_path(some_note):
+    assert some_note.full_path == Path('/test/path/2024-08-01/uri/fake.note')
+
+
+def test_file_bytes(some_note):
+    assert some_note.file_bytes == b''
+
+
+def test_file_bytes_raises_typerror(some_note):
+    with pytest.raises(TypeError):
+        some_note.file_bytes = 'not_bytes_object'
+
+
+def test_file_bytes_sets_properly(some_note):
+    some_note.file_bytes = b'actual_bytes_object'
+    some_note.file_bytes == b'actual_bytes_object'
